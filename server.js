ccdd35321b96f33a8a5d0e48a6821d819959820a
celso <celso.sjunior@outlook.com>
2024-09-21 12:39:04 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+const path = require('path');
 const express = require('express');
 const axios = require('axios');
 
 const app = express();
 app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Example root route, serves your index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const apiKey = process.env.OPENAI_API_KEY;
 
